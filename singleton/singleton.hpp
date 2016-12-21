36a27d6015481394ec1f04b970ed7e40fdf18bb0
yanxin <359063370@qq.com>
2016-12-21 15:51:55 +0800

    单例的c++模板实现,线程安全

---
+++ singleton/singleton.hpp
@@ -0,0 +1,46 @@
+#include <mutex>
+
+// 定义跨平台的退出函数
+#define ATEXIT(param) atexit(param)
+
+template <typename T>
+class Singleton
+{
+public:
+    static T& getInstance()
+    {
+        if (m_pInstance == NULL)
+        {
+            std::mutex lock;
+            if (m_pInstance == NULL)
+            {
+                m_pInstance = new(std::nothrow) T();
+                m_pInstance->init();
+                ATEXIT(Destroy);
+            }
+            return *m_pInstance;
+        }
+        return *m_pInstance;
+    }
+    
+    
+    virtual void init(){}
+protected:
+    Singleton(void) {}
+    virtual ~Singleton(void) {}
+private:
+    Singleton(const Singleton& rhs) {}
+    Singleton& operator = (const Singleton& rhs) {}
+    
+    static void Destroy()
+    {
+        if (m_pInstance != NULL)
+            delete m_pInstance;
+        m_pInstance = NULL;
+    }
+    
+    static T* m_pInstance;
+};
+
+template <typename T>
+T* Singleton<T>::m_pInstance = NULL;
